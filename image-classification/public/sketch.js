a2d97a3289667a1b7571dba180703663f128c343
Mateus Tizotti <mtizotti@gmail.com>
2020-04-09 16:16:23 -0300

    Classification with saved images implemented

--- image-classification/public/sketch.js
+++ image-classification/public/sketch.js
@@ -1,9 +1,29 @@
-function setup() {
-    createCanvas(600, 600);
+let mobilenet;
+let img;
+
+function preload() {
+    mobilenet = ml5.imageClassifier('MobileNet', modelReady());
+    img = loadImage('../images/penguin.jpg');
 
 }
+function modelReady() {
+    console.log("Model is Ready!!");
+    
+}
 
-function draw() {
-    background(0);
+function setup() {
+    createCanvas(600, 600);
+    mobilenet.classify(img, (err, result) => {
+        if (err) {
+            console.error("Error!");
+        } else {
+            console.log(result);
+            let label = result[0].label;
+            let confidence = result[0].confidence * 100;
+            createDiv('Label: ' + label);
+            createDiv('Confidence: ' + nf(confidence, 2, 2) + '%');
+        }
+    });
 
+    image(img, 0, 0, width, height);
 }
